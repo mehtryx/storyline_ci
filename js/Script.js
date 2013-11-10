5c5fd2b2fefd22a429f9406529e15be230f1151a
W.A <waheez@Waheeds-MacBook-Pro.local>
2013-11-10 00:52:49 -0500

    Fixed the dynamic height of the footer and content of storyline within the preview window

--- js/Script.js
+++ js/Script.js
@@ -1,49 +1,69 @@
 
-//(function ($) {
-
-var devices = [
-        {
-            name: 'iPhone',
-            id: 'iphone',
-            width: 320,
-            height: 480
-        },
-        {
-            name: 'iPhone 4/4s',
-            width: 320,
-            height: 480,
-            id: 'iphone4'
-        },
-        {
-            name: 'iPhone 5/5s',
-            width: 320,
-            height: 564,
-            id: 'iphone5'
-        }
-];
-
-function PopulateDevicesList() {
-    var selector = jQuery("#phoneselector");
-    jQuery.each(devices, function (index, value) {
-        selector.append("<option value='" + value.id + "'>" + value.name + "</option>");
-    });
-    jQuery(".sub").css({ top:  413});
-}
-
-jQuery("#phoneselector").change(function () {
-    var selectedVal = jQuery(this).val();
-    device = jQuery.grep(devices, function (n,i) { return n.id ==  selectedVal})[0];
-    if (device != null) {
-        jQuery(".device-wrapper").animate({ width: device.width + 56 +'px', height: device.height + 148 + 'px' }, 500);
+(function ($) {
+    var devices = [
+            {
+                name: 'iPhone',
+                id: 'iphone',
+                width: 320,
+                height: 480
+            },
+            {
+                name: 'iPhone 4/4s',
+                width: 320,
+                height: 480,
+                id: 'iphone4'
+            },
+            {
+                name: 'iPhone 5/5s',
+                width: 320,
+                height: 564,
+                id: 'iphone5'
+            },
+            {
+                name: 'Nexus 4',
+                width: 384,
+                height: 640,
+                id: 'nexus4'
+            }
+    ];
+    var storyItemHeight = 480;
+    var storyItemWidth = 320;
+    function PopulateDevicesList() {
+        var selector = $("#phoneselector");
+        $.each(devices, function (index, value) {
+            selector.append("<option value='" + value.id + "'>" + value.name + "</option>");
+        });
+        
     }
-});
+    function adjustFooterPosition (height) {
+        $(".adBanner").css({top:height-495, width: '100%'});
+        $(".statusbar").css({top:height-135, width: '100%'});
+        $(".sub").css({top:height-90, width: '100%'});
+    }
+    new Swiper('.swiper-container', {
+        pagination: '.pagination',
+        paginationClickable: true,
+        mode: 'vertical',
+        width: '100%',
+    });
+    $(".sub").css({ top:  413});
+    $("#phoneselector").change(function () {
+        var selectedVal = $(this).val();
+        device = $.grep(devices, function (n,i) { return n.id ==  selectedVal})[0];
+        if (device != null) {
+            $(".device-wrapper").animate({ width: device.width + 56 +'px', height: device.height + 148 + 'px' }, 500);
+            adjustFooterPosition(device.height);
+            new Swiper('.swiper-container', {
+                pagination: '.pagination',
+                paginationClickable: true,
+                mode: 'vertical'
+            });
+        }
+    });
 
 
 
-var mySwiper = new Swiper('.swiper-container', {
-    pagination: '.pagination',
-    paginationClickable: true,
-    mode: 'vertical'
-});
-PopulateDevicesList();
-//})(jQuery);
+    
+    PopulateDevicesList();
+    adjustFooterPosition(480);
+})(jQuery);
