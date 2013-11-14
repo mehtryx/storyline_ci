2b0d0d304eff6b42ce73574d287959d3e3c0acde
Waheed Ashraf <waheed@Waheeds-MacBook-Pro.local>
2013-11-14 15:35:21 -0500

    Added json feed filters and fixed preview bugs

--- storyline.php
+++ storyline.php
@@ -32,39 +32,44 @@ TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-define( 'SMRT_STORYLINE_VERSION', '0.1.0');
+define( 'SMRT_STORYLINE_VERSION', '0.2.0');
 
 class SMRT_Storyline {
 
-
 	public function __construct() {
-
+		include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
 		// register new content type
 	    add_action( 'init', array( $this, 'register_storyline' ) );
         add_filter( 'the_content', array( $this, 'modified_post_view' )); 
+		if(is_plugin_active( 'json-feed/json-feed.php' ) )
+			add_filter('json_feed_item',  array($this ,'json_feed_items_with_slides'));
     }
     
+    public function json_feed_items_with_slides($item){
+		$item['content'] =   $this -> split_content($item['content']);
+		$item['last_modified'] = get_lastpostmodified();
+		return $item;
+    }
+    
+	function split_content($content){
+		$content = preg_replace('/<span id=\"more-.*\"><\/span>/u', "<!--more-->", $content);
+		return explode("<!--more-->", $content);
+	}
+	
 	public function register_storyline() {
 		register_post_type( 'storyline', array(
 			'public' => true,
-			'label' => 'Storylines',
-			'description' => 'Storylines',
+			'label' => 'Storyline',
+			'description' => 'Storyline',
 			'menu_position' => 5,
 			'has_archive' => true,
 			'rewrite' => array( 'slug' => 'storyline' ),
-			'supports' => array( 'title', 'editor', 'thumbnail', 'excerpt'),
+			'supports' => array( 'title', 'editor', 'thumbnail', 'excerpt','post-formats'),
 			'taxonomies' => array( 'category')
 		) );
 	}
-    public function startsWith($haystack, $needle)
-    {
-        return $needle === "" || strpos($haystack, $needle) === 0;
-    }
-    public function endsWith($haystack, $needle)
-    {
-        return $needle === "" || substr($haystack, -strlen($needle)) === $needle;
-    }
-    public function split_content_intoslides($slides)
+	
+    public function render_content_slides($slides)
     {        
         $statusBar = "<div class='x-unsized x-label bottom-pagination x-dock-item x-docked-bottom x-has-width' id='ext-label-1'><div class='x-innerhtml' id='ext-element-102'>1/3</div></div>";
         $slides_content = "";
@@ -78,14 +83,16 @@ class SMRT_Storyline {
         }
         return $slides_content;
     }
+	
     public function modified_post_view($content)
     {
         global $post;
         if($post->post_type != 'storyline')
             return $content;
         
-        $header = "<div class='x-container x-toolbar-dark x-toolbar x-navigation-bar x-stretched x-paint-monitored x-layout-box-item' id='ext-titlebar-1'><div class='x-body' id='ext-element-13'><div class='x-center' id='ext-element-14'><div class='x-unsized x-title x-floating' id='ext-title-1' style='z-index: 8 !important;'><div class='x-innerhtml' id='ext-element-12'></div></div></div><div class='x-inner x-toolbar-inner x-horizontal x-align-stretch x-pack-start x-layout-box' id='ext-element-9'><div class='x-container x-size-monitored x-paint-monitored x-layout-box-item x-stretched' id='ext-container-1' style='position: relative;'><div class='x-inner x-horizontal x-align-center x-pack-start x-layout-box' id='ext-element-10' style=''><div class='x-button x-iconalign-center x-button-plain x-layout-box-item x-stretched' id='nav_btn'><span id='ext-element-15' class='x-badge' style='display: none;'></span><span class='x-button-icon x-shown list' id='ext-element-17'></span><span id='ext-element-16' class='x-button-label' style='display: none;'></span></div><div class='x-img x-img-image x-img-background x-sized x-paint-monitored x-size-monitored x-layout-box-item' id='nav_icon' style='width: 42px !important; height: 48px !important;'><div class='x-paint-monitor overflowchange'></div><div class='x-size-monitors overflowchanged'><div class='expand'><div style='width: 43px; height: 49px;'></div></div><div class='shrink'><div style='width: 42px; height: 48px;'></div></div></div></div></div><div class='x-size-monitors overflowchanged'><div class='expand'><div style='width: 95.859375px; height: 49px;'></div></div><div class='shrink'><div style='width: 94.859375px; height: 48px;'></div></div></div><div class='x-paint-monitor overflowchange'></div></div><div class='x-size-monitored x-paint-monitored x-layout-box-item x-flexed x-stretched' id='ext-component-1' style='position: relative; -webkit-box-flex: 1;'><div class='x-size-monitors overflowchanged'><div class='expand'><div style='width: 247px; height: 49px;'></div></div><div class='shrink'><div style='width: 246px; height: 48px;'></div></div></div><div class='x-paint-monitor overflowchange'></div></div><div class='x-container x-size-monitored x-paint-monitored x-layout-box-item x-stretched' id='ext-container-2' style='position: relative;'><div class='x-inner x-horizontal x-align-center x-pack-start x-layout-box' id='ext-element-11'><div class='x-button x-iconalign-center x-button-plain x-layout-box-item x-stretched' id='bt_alert'><span class='x-badge' style='display: none;'></span><span class='x-button-icon x-shown info' id='ext-element-18'></span><span class='x-button-label' style='display: none;'></span></div></div><div class='x-size-monitors overflowchanged'><div class='expand'><div style='width: 53.859375px; height: 49px;'></div></div><div class='shrink'><div style='width: 52.859375px; height: 48px;'></div></div></div><div class='x-paint-monitor overflowchange'></div></div></div></div><div class='x-paint-monitor overflowchange'></div></div>";
-        $adBanner = "<div class='adBanner x-img x-img-image x-sized x-paint-monitored x-size-monitored bottom_ad x-img-background x-dock-item x-docked-bottom' id='ext-image-2' style='width: 100% !important; height: 50px !important;'><div class='x-paint-monitor overflowchange'></div><div class='x-size-monitors overflowchanged'><div class='expand'><div style='width: 402px; height: 51px;'></div></div><div class='shrink'><div style='width: 100%; height: 50px;'></div></div></div></div>";
+       
+        $header = "<div class='x-container x-toolbar-dark x-toolbar x-navigation-bar x-stretched x-paint-monitored x-layout-box-item' id='ext-titlebar-1'><div class='x-body' id='ext-element-13'><div class='x-center' id='ext-element-14'><div class='x-unsized x-title x-floating' id='ext-title-1' style='z-index: 8 !important;'><div class='x-innerhtml' id='ext-element-12'></div></div></div><div class='x-inner x-toolbar-inner x-horizontal x-align-stretch x-pack-start x-layout-box' id='ext-element-9'><div class='x-container x-size-monitored x-paint-monitored x-layout-box-item x-stretched' id='ext-container-1' style='position: relative;'><div class='x-inner x-horizontal x-align-center x-pack-start x-layout-box' id='ext-element-10' style=''><div class='x-img x-img-image x-img-background x-sized x-paint-monitored x-size-monitored x-layout-box-item' id='nav_btn' style='width: 50px !important; height: 50px !important; background-image: url(./wp-content/plugins/storyline/img/toolbar/icon-menu.png);'><div class='x-paint-monitor overflowchange'></div><div class='x-size-monitors overflowchanged'><div class='expand'><div style='width: 51px; height: 51px;'></div></div><div class='shrink'><div style='width: 50px; height: 50px;'></div></div></div></div><div class='x-img x-img-image x-img-background x-sized x-paint-monitored x-size-monitored x-layout-box-item' id='nav_icon' style='width: 42px !important; height: 48px !important; background-image: url(./wp-content/plugins/storyline/img/logo.png);'><div class='x-paint-monitor overflowchange'></div><div class='x-size-monitors overflowchanged'><div class='expand'><div style='width: 43px; height: 49px;'></div></div><div class='shrink'><div style='width: 42px; height: 48px;'></div></div></div></div></div><div class='x-size-monitors overflowchanged'><div class='expand'><div style='width: 93px; height: 51px;'></div></div><div class='shrink'><div style='width: 92px; height: 50px;'></div></div></div><div class='x-paint-monitor overflowchange'></div></div><div class='x-size-monitored x-paint-monitored x-layout-box-item x-flexed x-stretched' id='ext-component-1' style='position: relative; -webkit-box-flex: 1;'><div class='x-size-monitors overflowchanged'><div class='expand'><div style='width: 1128.71875px; height: 51px;'></div></div><div class='shrink'><div style='width: 1127.71875px; height: 50px;'></div></div></div><div class='x-paint-monitor overflowchange'></div></div><div class='x-container x-size-monitored x-paint-monitored x-layout-box-item x-stretched' id='ext-container-2' style='position: relative;'><div class='x-inner x-horizontal x-align-center x-pack-start x-layout-box' id='ext-element-11' style=''><div class='x-img x-img-image x-img-background x-sized x-paint-monitored x-size-monitored x-layout-box-item' id='bt_alert' style='width: 50px !important; height: 50px !important; background-image: url(./wp-content/plugins/storyline/img/toolbar/icon-alerts-active.png);'><div class='x-paint-monitor overflowchange'></div><div class='x-size-monitors overflowchanged'><div class='expand'><div style='width: 51px; height: 51px;'></div></div><div class='shrink'><div style='width: 50px; height: 50px;'></div></div></div></div></div><div class='x-size-monitors overflowchanged'><div class='expand'><div style='width: 51px; height: 51px;'></div></div><div class='shrink'><div style='width: 50px; height: 50px;'></div></div></div><div class='x-paint-monitor overflowchange'></div></div></div></div><div class='x-paint-monitor overflowchange'></div></div>";
+		$adBanner = "<div class='adBanner x-img x-img-image x-sized x-paint-monitored x-size-monitored bottom_ad x-img-background x-dock-item x-docked-bottom' id='ext-image-2' style='width: 100% !important; height: 50px !important;'><div class='x-paint-monitor overflowchange'></div><div class='x-size-monitors overflowchanged'><div class='expand'><div style='width: 402px; height: 51px;'></div></div><div class='shrink'><div style='width: 100%; height: 50px;'></div></div></div></div>";
        
         wp_register_style( 'storylinestyle', plugins_url('css/style.css', __FILE__) );
         wp_register_style( 'swipercss', plugins_url('css/idangerous.swiper.css', __FILE__) );
@@ -97,7 +104,7 @@ class SMRT_Storyline {
         wp_enqueue_script( 'swiperscript' );
         wp_enqueue_script( 'storylinescript' );
         
-        $slides = explode("<!--more-->", str_replace("<span id=\"more-4\"></span>", "<!--more-->", $content));
+        $slides = $this->split_content($content);
         return "<div class='smart-device-preview'>
                     <div class='select-device'> 
                         <label for='phoneselector'>Select a device</label><br>
@@ -117,7 +124,7 @@ class SMRT_Storyline {
                                             <div class='story_abstract'>". $slides[0] ."</div></div>
                                         </div><div class='statusbar'>1/".(count($slides))."</div>".$adBanner.
                                     "</div>"
-                                    .$this->split_content_intoslides($slides).
+                                    .$this->render_content_slides($slides).
                                 "</div>
                                 <div class='pagination'></div>
                             <div>
