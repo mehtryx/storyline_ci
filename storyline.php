c65b6ea0fc728f5af9b59d05ad8a5d6df3fbc89b
Edward de Groot <ed@degroot.ca>
2013-12-06 14:29:56 -0500

    fixed spacing for vip

--- storyline.php
+++ storyline.php
@@ -175,8 +175,8 @@ class SMRT_Storyline {
             'menu_position' => 5,
             'has_archive' => true,
             'rewrite' => array( 'slug' => 'storyline' ),
-            'supports' => array( 'title', 'editor', 'thumbnail', 'excerpt','post-formats'),
-			'taxonomies' => array( 'category')
+            'supports' => array( 'title', 'editor', 'thumbnail', 'excerpt','post-formats' ),
+			'taxonomies' => array( 'category' )
         ) );
 		
 		$labels = array(
