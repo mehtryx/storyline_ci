4a6f7fd0fa12b5783fc5259b9c0c570761d4e182
dmarges <donniemarges@gmail.com>
2014-01-02 16:10:18 -0500

    added check for post format to only include in feed when not false

--- storyline.php
+++ storyline.php
@@ -90,7 +90,9 @@ class SMRT_Storyline {
 
         $item['content'] = $this->split_content( $item['content'], true );
         $item['last_modified'] = get_the_modified_time(json_feed_date_format());
-        $item['post_format'] = get_post_format();
+
+        if( false !== get_post_format() )
+        	$item['post_format'] = get_post_format();
 		
 		// calculate index of post
 		static $offset;
