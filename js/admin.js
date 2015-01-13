35341619f6816b6b804596af1cf95425bcb5f629
Keith Benedict <keith@benedicthome.com>
2015-01-13 08:38:03 -0500

    reduced extrenuous files to focus on testing

--- js/admin.js
+++
@@ -1,6 +0,0 @@
-jQuery( document ).ready( function() {
-	jQuery( "#sc_select_storyline" ).change( function() {
-			send_to_editor( jQuery( "#sc_select_storyline :selected" ).val() );
-			return false;
-	} );
-} );
--- js/quick-edit.js
+++
@@ -1,7 +0,0 @@
-jQuery(document).ready(function() {
-    jQuery('.editinline').on('click', function() {
-        var tag_id = jQuery(this).parents('tr').attr('id');
-        var term_group = jQuery('.term_group', '#' + tag_id).text();
-        jQuery(':input[name="term_group"]', '.inline-edit-row').val(term_group);
-    });
-});
